62f0acc8e7240ac8fbb41aef17ea47cc0841a8f8
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-07-19 23:57:25 +0545

    test: mostBlogs, totalLikes, dummy, favoriteBlog

--- tests/dummy.test.js
+++ tests/dummy.test.js
@@ -1,6 +1,6 @@
 const list_helper = require('../utils/list_helper')
 
-test('dummy test', () => {
+test('dummy resturns 1', () => {
   const blogs = []
   const result = list_helper.dummy(blogs)
   expect(result).toBe(1)
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,31 @@
-const dummy = (blogs) => {
+const dummy = (array) => {
   return 1
+}
+
+const totalLikes = (array) => {
+  let arr = []
+
+  const likesArray = () => {
+    array.forEach((a) => arr.push(a.likes))
+    return arr
+  }
+  likesArray()
+
+  return arr.length === 1 ? arr[0] : arr.reduce((s, p) => s + p, 0)
+}
+
+const favoriteBlog = (array) => {
+  
+}
+
+const mostBlogs = (array) => {
+  const authors = array.sort((a, b) => a.author === b.author)
+  return { author: `${authors[0].author}`, blogs: authors.length }
 }
 
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
 }
